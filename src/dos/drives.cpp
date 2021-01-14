5d8e0ae96b09cb3f1c759f10173c7d2042039673
Bernhard Schelling <14200249+schellingb@users.noreply.github.com>
2021-01-15 04:22:56 +0900

    A new and hopeful better year

--- src/dos/drives.cpp
+++ src/dos/drives.cpp
@@ -1,6 +1,6 @@
 /*
  *  Copyright (C) 2002-2021  The DOSBox Team
- *  Copyright (C) 2020-2020  Bernhard Schelling
+ *  Copyright (C) 2020-2021  Bernhard Schelling
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
