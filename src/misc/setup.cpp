f5aa6755d281772083a686ea69e84c9f305a7d3d
Bernhard Schelling <14200249+schellingb@users.noreply.github.com>
2021-02-24 03:51:10 +0900

    Update DOSBox SVN Merge revisions 4425 ~ 4441

--- src/misc/setup.cpp
+++ src/misc/setup.cpp
@@ -745,8 +745,10 @@ string Section_line::GetPropValue(string const& /* _property*/) const {
 }
 
 #ifdef C_DBP_ENABLE_CONFIG_PROGRAM
+#define HELPLINE_SIZE 256
 bool Config::PrintConfig(char const * const configfilename) const {
-	char temp[50];char helpline[256];
+	char temp[50];
+	char helpline[HELPLINE_SIZE] = {0};
 	FILE* outfile = fopen(configfilename,"w+t");
 	if (outfile == NULL) return false;
 
@@ -801,7 +803,7 @@ bool Config::PrintConfig(char const * const configfilename) const {
 			const char * helpstr = MSG_Get(temp);
 			const char * linestart = helpstr;
 			char * helpwrite = helpline;
-			while (*helpstr && helpstr - linestart < sizeof(helpline)) {
+			while (*helpstr && helpstr - linestart < HELPLINE_SIZE - 2) {
 				*helpwrite++ = *helpstr;
 				if (*helpstr == '\n') {
 					*helpwrite = 0;
