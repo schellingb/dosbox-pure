01f2f5f6f3b5c2c2c58fbd4be7c271542aa84155
Bernhard Schelling <14200249+schellingb@users.noreply.github.com>
2021-10-21 04:12:33 +0900

    Increase version number

--- dosbox_pure_libretro.cpp
+++ dosbox_pure_libretro.cpp
@@ -2129,7 +2129,7 @@ void retro_get_system_info(struct retro_system_info *info) // #1
 {
 	memset(info, 0, sizeof(*info));
 	info->library_name     = "DOSBox-pure";
-	info->library_version  = "0.19";
+	info->library_version  = "0.20";
 	info->need_fullpath    = true;
 	info->block_extract    = true;
 	info->valid_extensions = "zip|dosz|exe|com|bat|iso|cue|ins|img|ima|vhd|m3u|m3u8";
