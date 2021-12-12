schellingb/dosbox-pure

commit 0e6bb1a2c15a1426d07bba9a1c3a1843e315efe5

Show "restart required" notification on more core options

dosbox_pure_libretro.cpp
@@ -2359,34 +2359,34 @@ static bool check_variables()
 		visibility_changed = true;
 	}
 
-	if (dbp_state == DBPSTATE_BOOT)
-	{
-		const char* machine = Variables::RetroGet("dosbox_pure_machine", "svga");
-		dbp_last_machine = machine[0];
-		if (!strcmp(machine, "svga"))
-			machine = Variables::RetroGet("dosbox_pure_svga", "svga_s3");
-		else if (!strcmp(machine, "vga"))
-			machine = "vgaonly";
-		Variables::DosBoxSet("dosbox", "machine", machine);
+	dbp_auto_mapping_mode = Variables::RetroGet("dosbox_pure_auto_mapping", "true")[0];
 
-		const char* audiorate = Variables::RetroGet("dosbox_pure_audiorate", DBP_DEFAULT_SAMPLERATE_STRING);
-		Variables::DosBoxSet("mixer",    "rate",      audiorate);
-		Variables::DosBoxSet("sblaster", "oplrate",   audiorate);
-		Variables::DosBoxSet("speaker",  "pcrate",    audiorate);
-		Variables::DosBoxSet("speaker",  "tandyrate", audiorate);
-
-		dbp_auto_mapping_mode = Variables::RetroGet("dosbox_pure_auto_mapping", "true")[0];
-
-		// initiate audio buffer, we don't need SDL specific behavior, so just set a large enough buffer
-		Variables::DosBoxSet("mixer", "prebuffer", "0");
-		Variables::DosBoxSet("mixer", "blocksize", "2048");
-	}
+	const char* machine = Variables::RetroGet("dosbox_pure_machine", "svga");
+	if (!strcmp(machine, "svga")) machine = Variables::RetroGet("dosbox_pure_svga", "svga_s3");
+	else if (!strcmp(machine, "vga")) machine = "vgaonly";
+	Variables::DosBoxSet("dosbox", "machine", machine, false, true);
 
 	const char* mem = Variables::RetroGet("dosbox_pure_memory_size", "16");
 	bool mem_use_extended = (atoi(mem) > 0);
 	Variables::DosBoxSet("dos", "xms", (mem_use_extended ? "true" : "false"), true);
 	Variables::DosBoxSet("dos", "ems", (mem_use_extended ? "true" : "false"), true);
 	Variables::DosBoxSet("dosbox", "memsize", (mem_use_extended ? mem : "16"), false, true);
+
+	const char* audiorate = Variables::RetroGet("dosbox_pure_audiorate", DBP_DEFAULT_SAMPLERATE_STRING);
+	Variables::DosBoxSet("mixer", "rate", audiorate, false, true);
+
+	if (dbp_state == DBPSTATE_BOOT)
+	{
+		dbp_last_machine = machine[0];
+
+		Variables::DosBoxSet("sblaster", "oplrate",   audiorate);
+		Variables::DosBoxSet("speaker",  "pcrate",    audiorate);
+		Variables::DosBoxSet("speaker",  "tandyrate", audiorate);
+
+		// initiate audio buffer, we don't need SDL specific behavior, so just set a large enough buffer
+		Variables::DosBoxSet("mixer", "prebuffer", "0");
+		Variables::DosBoxSet("mixer", "blocksize", "2048");
+	}
 
 	// handle setting strings like on/yes/true/savestate or rewind
 	const char* savestate = Variables::RetroGet("dosbox_pure_savestate", "false");
@@ -2407,7 +2407,6 @@ static bool check_variables()
 	Variables::DosBoxSet("cpu", "core",    Variables::RetroGet("dosbox_pure_cpu_core", "auto"), false, true);
 	Variables::DosBoxSet("cpu", "cputype", Variables::RetroGet("dosbox_pure_cpu_type", "auto"), false, true);
 
-	const char* machine = Variables::RetroGet("dosbox_pure_machine", "svga");
 	if (dbp_last_machine != machine[0])
 	{
 		dbp_last_machine = machine[0];

commit f440e9717388d485593eda6bc79a8980dc014176

Fix 'SVGA Mode' core option not showing Option stopped showing due to regression in 0e6bb1a (#215)

dosbox_pure_libretro.cpp
@@ -2487,6 +2487,7 @@ static bool check_variables()
 	dbp_auto_mapping_mode = Variables::RetroGet("dosbox_pure_auto_mapping", "true")[0];
 
 	const char* machine = Variables::RetroGet("dosbox_pure_machine", "svga");
+	bool machine_is_svga = !strcmp(machine, "svga"), machine_is_cga = !strcmp(machine, "cga"), machine_is_hercules = !strcmp(machine, "hercules");
 	if (!strcmp(machine, "svga")) machine = Variables::RetroGet("dosbox_pure_svga", "svga_s3");
 	else if (!strcmp(machine, "vga")) machine = "vgaonly";
 	Variables::DosBoxSet("dosbox", "machine", machine, false, true);
@@ -2563,10 +2564,8 @@ static bool check_variables()
 		visibility_changed = true;
 	}
 
-	bool machine_is_svga = !strcmp(machine, "svga");
 	Variables::RetroVisibility("dosbox_pure_svga", machine_is_svga);
 
-	bool machine_is_cga = !strcmp(machine, "cga");
 	Variables::RetroVisibility("dosbox_pure_cga", machine_is_cga);
 	if (machine_is_cga)
 	{
@@ -2578,7 +2577,6 @@ static bool check_variables()
 		DBP_CGA_SetModelAndComposite(cga_new_model, (!cga_mode || cga_mode[0] == 'a' ? 0 : ((cga_mode[0] == 'o' && cga_mode[1] == 'n') ? 1 : 2)));
 	}
 
-	bool machine_is_hercules = !strcmp(machine, "hercules");
 	Variables::RetroVisibility("dosbox_pure_hercules", machine_is_hercules);
 	if (machine_is_hercules)
 	{