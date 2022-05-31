3492f24ecc711148ca82bb66fcdf519e4fb01fbe
Bernhard Schelling <14200249+schellingb@users.noreply.github.com>
2022-05-31 18:32:40 +0900

    Set version to 0.9.3

--- dosbox_pure_libretro.cpp
+++ dosbox_pure_libretro.cpp
@@ -2649,7 +2649,7 @@ void retro_get_system_info(struct retro_system_info *info) // #1
 {
 	memset(info, 0, sizeof(*info));
 	info->library_name     = "DOSBox-pure";
-	info->library_version  = "0.9.2";
+	info->library_version  = "0.9.3";
 	info->need_fullpath    = true;
 	info->block_extract    = true;
 	info->valid_extensions = "zip|dosz|exe|com|bat|iso|cue|ins|img|ima|vhd|jrc|tc|m3u|m3u8|conf";
