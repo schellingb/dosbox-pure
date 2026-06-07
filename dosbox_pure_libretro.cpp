192eef8b78919fdb4b64afcd4581601535f4077a
finalpatch <fengli@gmail.com>
2026-06-07 10:31:29 +1000

    revert version string

--- dosbox_pure_libretro.cpp
+++ dosbox_pure_libretro.cpp
@@ -2114,7 +2114,7 @@ void retro_get_system_info(struct retro_system_info *info) // #1
 {
 	memset(info, 0, sizeof(*info));
 	info->library_name     = "DOSBox-pure";
-	info->library_version  = "0.9.9-finalpatch";
+	info->library_version  = DOSBOX_PURE_VERSION_STR;
 	info->need_fullpath    = true;
 	info->block_extract    = true;
 	info->valid_extensions = "zip|dosz|exe|com|bat|iso|chd|cue|ins|img|ima|vhd|jrc|m3u|m3u8|conf|/";
